e171027787f68a65af7481cd67e44c3970bfb658
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-08-31 22:14:22 +0200

    button components

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  @Input() text: string = 'New';
+  @Input() backgroundColor: string = 'bg-purple';
+  @Input() textColor: string = 'text-white';
+}
