db0b9abe1b2232d0f8672279b52c06acd79d7985
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-08-31 22:17:07 +0200

    invoice details components

---
+++ src/app/components/invoice-details/invoice-details.component.ts
@@ -0,0 +1,50 @@
+import { selectInvoiceById } from '../../store/invoice/invoice.selectors';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Invoice } from '../../models/invoice.model';
+import { Store } from '@ngrx/store';
+import { InvoiceState } from '../../store/invoice/invoice.reducer';
+import { Observable } from 'rxjs';
+import * as InvoiceActions from '../../store/invoice/invoice.actions';
+
+@Component({
+  selector: 'app-invoice-details',
+  templateUrl: './invoice-details.component.html',
+  styleUrl: './invoice-details.component.css',
+})
+export class InvoiceDetailsComponent implements OnInit {
+  showModal: boolean = false;
+  invoice$!: Observable<Invoice | undefined>;
+
+  deleteInvoice() {
+    this.showModal = true;
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<{ invoice: InvoiceState }>,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const idParam = params.get('id');
+      if (idParam) {
+        this.invoice$ = this.store.select(selectInvoiceById(idParam));
+      }
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/home']);
+  }
+
+  markAsPaid(invoice: Invoice) {
+    if (invoice.status !== 'paid') {
+      const updatedInvoice = { ...invoice, status: 'paid' };
+      this.store.dispatch(
+        InvoiceActions.updateInvoice({ invoice: updatedInvoice })
+      );
+    }
+  }
+}
